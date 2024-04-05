d4f5e3f6ee2f21915a31ad3126606e211080c996
0xkuwabatake <0xkuwabatake@gmail.com>
2024-04-05 17:56:11 +0700

    custom errors & events from parent contracts added

--- test/examples/SimpleERC721T.t.sol
+++ test/examples/SimpleERC721T.t.sol
@@ -8,6 +8,15 @@ import {SimpleERC721T} from "src/examples/SimpleERC721T.sol";
 contract SimpleERC721TTest is Test {
     SimpleERC721T public simpleERC721T;
 
+    error TokenDoesNotExist();
+    error TierDoesNotExist();
+    error URICanNotBeEmptyString();
+    error ArrayLengthsMismatch();
+
+    event TierMinted (uint256 indexed tokenId, uint256 indexed tierId);
+    event TierBurned (uint256 indexed tokenId, uint256 indexed tierId);
+    event TierURI (string tierURI, uint256 indexed tier);
+
     function setUp() external {
         simpleERC721T = new SimpleERC721T();
     }
