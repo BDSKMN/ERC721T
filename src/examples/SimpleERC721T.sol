21f0fe34777ac70a57cf8f68629893ee0dea41a4
0xkuwabatake <0xkuwabatake@gmail.com>
2024-04-08 16:57:37 +0700

    add inheritance to {Ownable}

--- src/examples/SimpleERC721T.sol
+++ src/examples/SimpleERC721T.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.20;
 
 import {ERC721T} from "src/ERC721T.sol";
+import {Ownable} from "lib/solady/src/auth/Ownable.sol";
 
-contract SimpleERC721T is ERC721T {
+contract SimpleERC721T is ERC721T, Ownable {
     error ArrayLengthsMismatch();
 
-    constructor() ERC721T("Simple ERC721T", "S721T") {}
+    constructor() ERC721T("Simple ERC721T", "S721T") {
+        _initializeOwner(msg.sender);
+    }
 
     function getTierId(uint256 tokenId) public view returns (uint256) {
         if (!_exists(tokenId)) revert TokenDoesNotExist();
@@ -22,7 +25,7 @@ contract SimpleERC721T is ERC721T {
         _mintTier(to, tierId);
     }
 
-    function airdrop(address[] calldata receivers, uint256 tierId) public {
+    function airdrop(address[] calldata receivers, uint256 tierId) public onlyOwner {
         for (uint256 i = 0; i < receivers.length;) {
             if (bytes(_tierURI[tierId]).length == 0) revert TierDoesNotExist();
             _mintTier(receivers[i], tierId);
@@ -34,7 +37,7 @@ contract SimpleERC721T is ERC721T {
         _burnTier(owner, tokenId);
     }
 
-    function batchBurn(address[] calldata owners, uint256[] calldata tokenIds) public {
+    function batchBurn(address[] calldata owners, uint256[] calldata tokenIds) public onlyOwner {
         if (owners.length != tokenIds.length) revert ArrayLengthsMismatch();
         for (uint256 i = 0; i < owners.length;) {
             _burnTier(owners[i], tokenIds[i]);
@@ -42,7 +45,7 @@ contract SimpleERC721T is ERC721T {
         }
     }
 
-    function setTierURI(uint256 tierId, string calldata tierURI) public {
+    function setTierURI(uint256 tierId, string calldata tierURI) public onlyOwner {
         _setTierURI(tierId, tierURI);
     }
 }
